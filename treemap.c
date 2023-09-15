9107abfa9926ad4c9e33e05034ba2c965f83aa86
Replit user <>
2023-09-15 02:28:10 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -59,7 +59,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value){
 
         if( aux == 0)return; //la clave ya existe
     
-        if (comp < 0) {
+        if (aux < 0) {
             parent = current;
             current = current->left; 
         } else {
